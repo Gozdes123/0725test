32a8ca0bae0f9749400a08a8063e0001804b80de
WilsonCheng <reborn2616am@gmail.com>
2022-07-25 15:23:14 +0800

    sas

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -12,12 +12,14 @@ class SiteController extends Controller
     {
         return "就我在搞";
     }
-    public function test03(){
-      return "別搞啊";
+    public function test03()
+    {
+        return "別搞啊";
 
     }
-    public function test04(){
+    public function test04()
+    {
         return "bug 分支";
-  
-      }
+
+    }
 }
