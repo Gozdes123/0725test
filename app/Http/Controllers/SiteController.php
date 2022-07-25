c4f92e25076493ff9c7cca78717f262ef9dbfc03
( ━☞´◔‿ゝ◔`)━☞ <9999999@gmail.com>
2022-07-25 14:28:57 +0800

    ㄏㄏ

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -10,7 +10,7 @@ class SiteController extends Controller
     }
     public function test02()
     {
-        return "我是凱  我在亂搞";
+        return "就我在搞";
     }
     public function test03(){
       return "別搞啊";
